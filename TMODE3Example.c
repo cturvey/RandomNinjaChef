49fd39cfea148311a7d723f05744b5928c99f8da
Clive Turvey <sourcer32@gmail.com>
2023-03-31 08:42:23 -0500

    Added in ECEF HP version

--- TMODE3Example.c
+++ TMODE3Example.c
@@ -54,7 +54,7 @@ void ConfigTMODE3LLA(FIFO *Fifo, double lat, double lon, double alt)
     0x00,0x00,0x00,0x00, // Y Lon
     0x00,0x00,0x00,0x00, // Z Alt
     0x00,0x00,0x00,0x00, // HP X,Y,Z / Reserved2
-    0xF4,0x01,0x00,0x00, // Acc (0.1 mm) 5 cm
+    0xF4,0x01,0x00,0x00, // Acc (0.1 mm) 500 = 5 cm
     0x00,0x00,0x00,0x00, // svinMinDur (seconds/samples)
     0x00,0x00,0x00,0x00, // svinAccLimit (0.1 mm)
     0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00, // Reserved3
@@ -63,7 +63,7 @@ void ConfigTMODE3LLA(FIFO *Fifo, double lat, double lon, double alt)
   int32_t x, y, z;
 
   x = (int32_t)(lat * 1e7); // Rough Integers
-  y = (int32_t)(lon * 1e7);
+  y = (int32_t)(lon * 1e7); // deg 7dp
   z = (int32_t)(alt * 1e2); // m to cm
 
 #if 0
@@ -80,13 +80,66 @@ void ConfigTMODE3LLA(FIFO *Fifo, double lat, double lon, double alt)
 
   lat -= (double)x * 1e-7;
   lon -= (double)y * 1e-7;
-  alt -= (double)z * 1e-2;
+  alt -= (double)z * 1e-2; // cm to m
 
   // Compute High-Precision (HP) +/-99 values (might get 100 with rounding)
 
   x = (int32_t)(lat * 1e9); // Fine Integers
-  y = (int32_t)(lon * 1e9);
+  y = (int32_t)(lon * 1e9); // deg 9dp
   z = (int32_t)(alt * 1e4); // m to 0.1 mm
+
+  ubx_cfg_tmode3[6 + 16] = x; // High Precision Bytes
+  ubx_cfg_tmode3[6 + 17] = y;
+  ubx_cfg_tmode3[6 + 18] = z;
+
+  SendUBLOX(Fifo, sizeof(ubx_cfg_tmode3), ubx_cfg_tmode3); // Computes Fletcher sums on packet, and dispatches
+}
+
+//****************************************************************************
+
+void ConfigTMODE3ECEF(FIFO *Fifo, double ex, double ey, double ez) // ECEF metres
+{
+  uint8_t ubx_cfg_tmode3[] = { // CFG-TMODE3 (06 71)  sourcer32@gmail.com
+    0xB5,0x62,0x06,0x71,0x28,0x00, // Header/Command/Size
+    0x00,0x00,           // Version=0, Reserved1
+    0x02,0x00,           // 2=Fixed, 0=ECEF
+    0x00,0x00,0x00,0x00, // X Lat
+    0x00,0x00,0x00,0x00, // Y Lon
+    0x00,0x00,0x00,0x00, // Z Alt
+    0x00,0x00,0x00,0x00, // HP X,Y,Z / Reserved2
+    0xF4,0x01,0x00,0x00, // Acc (0.1 mm) 500 = 5 cm
+    0x00,0x00,0x00,0x00, // svinMinDur (seconds/samples)
+    0x00,0x00,0x00,0x00, // svinAccLimit (0.1 mm)
+    0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00, // Reserved3
+    0xAA,0xAA }; // compute checksum
+
+  int32_t x, y, z;
+
+  x = (int32_t)(ex * 1e2); // Rough Integers
+  y = (int32_t)(ey * 1e2);
+  z = (int32_t)(ez * 1e2); // m to cm
+
+#if 0
+  *((int32_t *)&ubx_cfg_tmode3[6 +  4]) = x; // Course Precision Words
+  *((int32_t *)&ubx_cfg_tmode3[6 +  8]) = y;
+  *((int32_t *)&ubx_cfg_tmode3[6 + 12]) = z;
+#else // Portable on platforms with alignment issues, ie Cortex-M0
+  memcpy(&ubx_cfg_tmode3[6 +  4], &x, sizeof(int32_t));
+  memcpy(&ubx_cfg_tmode3[6 +  8], &y, sizeof(int32_t));
+  memcpy(&ubx_cfg_tmode3[6 + 12], &z, sizeof(int32_t));
+#endif
+
+  // Roll off low precision rough integers
+
+  ex -= (double)x * 1e-2;
+  ey -= (double)y * 1e-2;
+  ez -= (double)z * 1e-2; // cm to m
+
+  // Compute High-Precision (HP) +/-99 values (might get 100 with rounding)
+
+  x = (int32_t)(ex * 1e4); // Fine Integers
+  y = (int32_t)(ey * 1e4);
+  z = (int32_t)(ez * 1e4); // m to 0.1 mm
 
   ubx_cfg_tmode3[6 + 16] = x; // High Precision Bytes
   ubx_cfg_tmode3[6 + 17] = y;
