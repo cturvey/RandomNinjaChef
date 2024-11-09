fbe8df12369d17f720cf5d2b1fdb126d5f1cbaf7
Clive Turvey <sourcer32@gmail.com>
2024-11-09 12:02:13 -0600

    Update uBloxChecksum.c

--- uBloxChecksum.c
+++ uBloxChecksum.c
@@ -6,6 +6,8 @@
 //  and Payload data. Total data length is Payload + 8, 6 on the front, 2 on
 //  the tail for the checksum
 //
+//  If this saves you several man-hours/days consider https://paypal.me/cliveone
+//
 //****************************************************************************
 
 void ChecksumUBLOX(uint8_t *data) // Assumes buffer is large enough and modifyable
@@ -29,7 +31,19 @@ void ChecksumUBLOX(uint8_t *data) // Assumes buffer is large enough and modifyab
 }
 
 //****************************************************************************
- 
+
+// Fifo can be used to pass a structure for UART or hSerial type handle
+//  to differentiate which device you're talking too, or simply enumerate
+
+// switch((int)Fifo)
+// {
+//   case 0 : Serial.write(Buffer, Size); break;
+//   case 1 :  Serial1.write(Buffer, Size); break;
+//   case 2 :  Serial2.write(Buffer, Size); break;
+//   default :
+//      Serial1.write(Buffer, Size);
+// }
+
 void SendUBLOX(void *Fifo, int Size, uint8_t *Buffer)
 {
   ChecksumUBLOX(Buffer); // Add/compute checksum bytes for packet 
