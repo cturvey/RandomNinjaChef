4c9039c65b8196d1383d0f37c1b040553c4950fc
Clive Turvey <sourcer32@gmail.com>
2025-04-30 13:47:18 -0500

    Update uBloxHABceiling.c

--- uBloxHABceiling.c
+++ uBloxHABceiling.c
@@ -6,6 +6,8 @@
 
 // Setting AIRBOURNE 1G (DYNAMIC=6), 80 KM VERTICAL, 100 M/S HORIZONTAL DYNAMICS uBlox Series 7 and 8
 
+// If this saves you several man-hours/days consider https://paypal.me/cliveone
+
 uint8_t ubx_cfg_nav5[] = { // CFG-NAV5 (06 24)
 0xB5,0x62,0x06,0x24,0x24,0x00, // Header/Command/Size
 0x01,0x00,0x06,0x00,0x00,0x00,0x00,0x00, // Payload data (Dynamics flag, and Dynamic setting)
@@ -17,10 +19,13 @@ uint8_t ubx_cfg_nav5[] = { // CFG-NAV5 (06 24)
 
 SendUBLOX(hUARTGPS, sizeof(ubx_cfg_nav5), ubx_cfg_nav5);
 
-// For Ardunio use the form Serial.write(buffer, sizeof(buffer));
+// For Arduino use the form Serial.write(buffer, sizeof(buffer));
 //  to send checksumed packets to the receiver.
 
 uint8_t ubx_cfg_valset_dyn6[] = { // Series 9 and 10
   0xB5,0x62,0x06,0x8A,0x09,0x00,  // Header/Command/Size  UBX-CFG-VALSET (RAM)
   0x00,0x01,0x00,0x00,0x21,0x00,0x11,0x20,0x06, // Payload data (0x20110021 CFG-NAVSPG-DYNMODEL = 6)
   0xF2,0x4F };
+
+// For Checksum / Send mechanics see
+//   https://github.com/cturvey/RandomNinjaChef/blob/main/uBloxChecksum.c
